810c0b29158d32f5fa52128d54125fa0a42fa870
Mathéo Robert <matho.robert@gmail.com>
2020-07-05 00:39:16 +0200

    Added svg to prepare next parallax added about fix fullpage and added navigation styled dot started about section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
 const navSlide = () => {
   var burger = document.querySelector(".hamburger");
   var navbar = document.querySelector(".fullNavbar");
+  var homeNavlink = document.querySelector(".homeNavlink");
+  var aboutNavlink = document.querySelector(".aboutNavlink");
+  var worksNavLink = document.querySelector(".worksNavlink");
+  var contactNavLink = document.querySelector(".contactNavlink");
 
   burger.addEventListener("click", () => {
+    navbar.classList.toggle("fullNavbarActive");
+    burger.classList.toggle("toggle");
+  });
+
+  homeNavlink.addEventListener("click", () => {
+    navbar.classList.toggle("fullNavbarActive");
+    burger.classList.toggle("toggle");
+  });
+
+  aboutNavlink.addEventListener("click", () => {
+    navbar.classList.toggle("fullNavbarActive");
+    burger.classList.toggle("toggle");
+  });
+
+  worksNavLink.addEventListener("click", () => {
+    navbar.classList.toggle("fullNavbarActive");
+    burger.classList.toggle("toggle");
+  });
+
+  contactNavLink.addEventListener("click", () => {
     navbar.classList.toggle("fullNavbarActive");
     burger.classList.toggle("toggle");
   });
 };
 
 navSlide();
+
+//Initialisation de la fonction de l'API Javascript
+
+//API mise en commentaire pour éviter l'excès de requete API avant la version build
+
+// function initMap() {
+//   var epitech = { lat: 45.74671126902152, lng: 4.834958581829663 };
+//   var map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 15,
+//     center: epitech,
+//   });
+//   var marker = new google.maps.Marker({
+//     position: epitech,
+//     map: map,
+//   });
+// }
