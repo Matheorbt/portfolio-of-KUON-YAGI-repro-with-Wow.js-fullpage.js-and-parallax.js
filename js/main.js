4c84190accbbe85d4115bb6a0da33e7c4b9d4eac
Mathéo Robert <matho.robert@gmail.com>
2020-08-14 16:20:40 +0200

    delete about description bug on js

--- js/main.js
+++ js/main.js
@@ -70,10 +70,6 @@ const changeLanguage = (language) => {
     scrollDown.innerHTML = "descendre";
     aboutTitle.innerHTML = "A propos de moi";
     subtitleAbout.innerHTML = "ce que j'aime / fais";
-    knowMoreAbout.innerHTML = "En savoir plus" + '<div class="arrowAbout">' + '<svg width="25" height="50" viewBox="0 0 50 100" fill="none" xmlns="http://www.w3.org/2000/svg">' +
-      '< path d = "M49.2288 48.0065L4.57724 0.833028C3.52591 -0.277676 1.83982 -0.277676 0.788495 0.833028C-0.262832 1.94373 -0.262832 3.72505 0.788495 4.83575L43.5358 49.9974L0.788495 95.159C-0.262832 96.2697 -0.262832 98.051 0.788495 99.1617C1.30424 99.7066 1.99851 100 2.67295 100C3.34738 100 4.04166 99.7276 4.5574 99.1617L49.209 51.9883C50.2603 50.8985 50.2603 49.0962 49.2288 48.0065Z" fill = "#061D38" />' + '</div>';
-    aboutDescription.innerHTML =
-      "Étudiant à l'école d'ingénieurs Epitech en masterje suis passionné par la programmation et plus particulièrement par le développement web.";
   } else {
     descriptionHero.innerHTML = "passionate about web/frontend";
     homeNavlink.innerHTML = "<a href='" + "#aboutSection" + "'>HOME</a>";
@@ -84,14 +80,6 @@ const changeLanguage = (language) => {
     scrollDown.innerHTML = "scrolldown";
     aboutTitle.innerHTML = "About me";
     subtitleAbout.innerHTML = "what I like /" + "</br>" + "do";
-    knowMoreAbout.innerHTML = "Know more about me" + '<div class="arrowAbout">' + '<svg width="25" height="50" viewBox="0 0 50 100" fill="none" xmlns="http://www.w3.org/2000/svg">' + 
-      '< path d = "M49.2288 48.0065L4.57724 0.833028C3.52591 -0.277676 1.83982 -0.277676 0.788495 0.833028C-0.262832 1.94373 -0.262832 3.72505 0.788495 4.83575L43.5358 49.9974L0.788495 95.159C-0.262832 96.2697 -0.262832 98.051 0.788495 99.1617C1.30424 99.7066 1.99851 100 2.67295 100C3.34738 100 4.04166 99.7276 4.5574 99.1617L49.209 51.9883C50.2603 50.8985 50.2603 49.0962 49.2288 48.0065Z" fill = "#061D38" />' + '</div>';
-</svg >
-</div >';
-    aboutDescription.innerHTML =
-      '<mark class="aboutMark">"</mark >' +
-      "Student at the Epitech engineering school in master's program I'm passionate about programming and more particularly by web development." +
-      '<mark class="aboutMark"> "</mark>';
   }
 };
 changeLanguage();
