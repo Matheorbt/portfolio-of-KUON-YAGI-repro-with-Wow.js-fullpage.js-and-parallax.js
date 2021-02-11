a3306beae118524b72b61ba859d6a394c39c38ff
MATHEO ROBERT <matho.robert@gmail.com>
2021-02-11 12:07:03 +0100

    Updated CV + Added translation in english + Improved responsivity of about section

--- js/main.js
+++ js/main.js
@@ -49,30 +49,35 @@ const knowMore = () => {
 };
 
 const changeLanguage = (language) => {
+  let aboutTitle = document.querySelector(".aboutTitle");
+  let worksTitle = document.querySelector(".worksTitle");
+  let contactTitle = document.querySelector(".contactTitle");
   let descriptionHero = document.querySelector(".descriptionHero");
   let homeNavlink = document.querySelector(".homeNavlink");
   let aboutNavlink = document.querySelector(".aboutNavlink");
   let worksNavlink = document.querySelector(".worksNavlink");
   let contactNavlink = document.querySelector(".contactNavlink");
   let scrollDown = document.querySelector(".scrollDownVertical");
-  let aboutTitle = document.querySelector(".aboutTitle");
   let subtitleAbout = document.querySelector(".subtitleAbout");
   let aboutDescription = document.getElementById("#aboutDescriptionLanguage");
   let knowMoreAbout = document.querySelector(".knowMoreAbout");
-  let contactTitle = document.querySelector(".contactTitle");
+  let aboutKnowMoreDescription = document.querySelector(".aboutKnowMoreDescription");
+  let subtitleWorks = document.querySelector(".subtitleWorks");
 
   if (language === "french") {
     descriptionHero.innerHTML = "passioné par le web/frontend";
     homeNavlink.innerHTML = "<a href='" + "#aboutSection" + "'>ACCUEIL</a>";
     aboutNavlink.innerHTML = "<a href='" + "#aboutSection" + "'>A PROPOS</a>";
     worksNavlink.innerHTML = "<a href='" + "#worksSection" + "'>TRAVAIL</a>";
-    contactNavlink.innerHTML =
-      "<a href='" + "#contactSection" + "'>CONTACT</a>";
+    contactNavlink.innerHTML = "<a href='" + "#contactSection" + "'>CONTACT</a>";
     scrollDown.innerHTML = "descendre";
-    aboutTitle.innerHTML = "A propos</br>de moi";
+    aboutTitle.innerHTML = "A propos de moi";
+    worksTitle.innerHTML = "Travail";
     subtitleAbout.innerHTML = "ce que j'aime / fais";
     knowMoreAbout.innerHTML = "En savoir plus >";
     contactTitle.innerHTML = "Contactez moi";
+    aboutKnowMoreDescription.innerHTML = "Actuellement étudiant en première année au cursus PGE de l'école Epitech sur le campus de Lyon, j'ai décidé de suivre cette formation qui permet un apprentissage par soi-même, une organisation autonome de son emploi du temps et des projets en groupe.</br>De plus, je suis très intéressé par le développement web que j'ai découvert en dehors du cadre de mes études au collège, j'aime la créativité et le travail en groupe que ce domaine m'apporte.";
+    subtitleWorks.innerHTML = "mon portfolio / </br>mes sites";
   } else {
     descriptionHero.innerHTML = "passionate about web/frontend";
     homeNavlink.innerHTML = "<a href='" + "#aboutSection" + "'>HOME</a>";
@@ -85,6 +90,9 @@ const changeLanguage = (language) => {
     subtitleAbout.innerHTML = "what I like /" + "</br>" + "do";
     knowMoreAbout.innerHTML = "Know more >";
     contactTitle.innerHTML = "Contact me";
+    aboutKnowMoreDescription.innerHTML = "Currently a first year student in the PGE course at the Epitech school on the Lyon campus, I decided to follow this training which allows self-study, autonomous organisation of one's schedule and group projects.</br>Moreover, I am very interested in web development which I discovered outside the framework of my studies at the college, I like the creativity and group work that this field brings me."
+    worksTitle.innerHTML = "Works";
+    subtitleWorks.innerHTML = "my portfolio / </br> my sites";
   }
 };
 changeLanguage();
